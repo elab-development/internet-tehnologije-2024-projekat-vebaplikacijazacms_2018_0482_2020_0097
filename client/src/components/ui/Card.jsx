f7344a90d120467aa0adb8978965e2077d4da2e7
javoracgligorije <javoracgligorije@gmail.com>
2025-10-28 20:40:19 +0100

    Delete client directory

--- client/src/components/ui/Card.jsx
+++
@@ -1,8 +0,0 @@
-// Omotač za sekcije – bela kartica sa paddingom i shadowom.
-export default function Card({ children, className = '' }) {
-  return (
-    <div className={`bg-white rounded-xl shadow p-6 ${className}`}>
-      {children}
-    </div>
-  );
-}
--- client/src/components/ui/Input.jsx
+++
@@ -1,9 +0,0 @@
-// Stilizovani <input> element za forme.
-export default function Input({ className = '', ...props }) {
-  return (
-    <input
-      className={`w-full rounded-lg border border-gray-200 px-3 py-2 focus:border-blue-400 focus:ring-2 focus:ring-blue-300 outline-none ${className}`}
-      {...props}
-    />
-  );
-}
